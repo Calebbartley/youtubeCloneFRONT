37ed5913eeb1b2681f56c315b7ad9be46e07d759
calebbartley <calebbartley18@gmail.com>
2021-08-19 22:13:12 -0400

    Hello world printed to client

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,22 +1,29 @@
-import React,{useState, useEffect} from 'react';
-import axios from 'axios'
-import './App.css';
+import React, { useState, useEffect } from "react";
+// import Bootstrap from 'bootstrap';
+// import { SearchBar, VideoList } from "./components";
+import axios from 'axios';
+
+const App = (props) => {
+
+  const [video, setVideo] = useState([]);
+  // const [selectedVideo, setSelectedVideo] = useState({ id: {}, snippet: {} });
+
+  useEffect(()=> {
+    axios.get('https://www.youtube.com/embed/VIDEO_ID')
+    .then(response => setVideo(response.data, [props.index])
+    )},[video, props.index])
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+
+}
+export default App;
 
 
-class
 
 
-const Container  = (props) =>{
-  const [collections, setCollections] = useState({})
-};
 
-useEffect(()=> {
-  axios.get('http')
-  .then(response => setCollections(response.data{props.index})
-  )},[Watch the original if changes log. ... iE. collections,props.index]);
 
-return (
-  <div>
-
-  </div>
-)
