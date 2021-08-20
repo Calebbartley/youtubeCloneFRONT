38a776cd0f97d627976963d1be562ed2e3921935
calebbartley <calebbartley18@gmail.com>
2021-08-19 20:26:31 -0400

    added notes

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
 
-class App extends component {
-  render (){
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+class
 
-export default App;
+
+const Container  = (props) =>{
+  const [collections, setCollections] = useState({})
+};
+
+useEffect(()=> {
+  axios.get('http')
+  .then(response => setCollections(response.data{props.index})
+  )},[Watch the original if changes log. ... iE. collections,props.index]);
+
+return (
+  <div>
+
+  </div>
+)
