5ee07b36ce9661acc9dcb059b23d2d12784a29ba
asiaromaria <designsbyarc@gmail.com>
2021-08-23 19:54:25 -0700

    took out search for now & added new useEffect for all comments

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { useState, useEffect } from "react";
-import Bootstrap from 'bootstrap';
-import SearchBar from "./components/SearchBar/searchBar";
+// import SearchBar from "./components/SearchBar/searchBar";
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const App = (props) => {
 
-  const [video, setVideo] = useState([]);
-  // const [selectedVideo, setSelectedVideo] = useState({ id: {}, snippet: {} });
+  const App = (props) => {
 
-  useEffect(()=> {
-    axios.get('https://www.youtube.com/embed/VIDEO_ID')
-    .then(response => setVideo(response.data)
-    )},[])
-    console.log(video)
-  return (
-    <div>
-      <h1>Hello World {useEffect}</h1>
-      <SearchBar />
-    </div>
-  );
+    // const [video, setVideo] = useState(0);
+    const [comments, setComments] = useState([]);
 
+    useEffect(() => {
+      axios.get('http://localhost:7000/api/comments')
+      .then(response => setComments(response.data))
+    }, [])
+
+
+    // useEffect(()=> {
+    //   axios.get('https://www.youtube.com/embed/dQw4w9WgXcQ')
+    //   .then(response => setComments(response.data[props.index])
+    //   )},[comments, props.index])
+    //   console.log(video)
+    return (
+      <div>
+        {/* <h1>Hello World {useEffect}</h1> */}
+        <ul>
+          {comments.map((comment, index) => <li key={index}>{comment.userName} {comment.userComment} </li>)}
+        </ul>
+        {/* <SearchBar /> */}
+      </div>
+    );
 }
+
 export default App;
 
 
--- src/components/SearchBar/searchBar.js
+++ src/components/SearchBar/searchBar.js
@@ -1,19 +1,57 @@
-import React from 'react';
+// import React from 'react';
 
 
-const SearchBar = () => (
-    <form action="/" method="get">
-        <label htmlFor="header-search">
-            <span className="visually-hidden">Search blog posts</span>
-        </label>
-        <input
-            type="text"
-            id="header-search"
-            placeholder="Search"
-            name="s" 
-        />
-        <button type="submit">Search</button>
+// const SearchBar = () => (
+//     <div class="dropdown">
+//   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+//     Dropdown button
+//   </button>
+//   <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+//     <li><a class="dropdown-item" href="#">Action</a></li>
+//     <li><a class="dropdown-item" href="#">Another action</a></li>
+//     <li><a class="dropdown-item" href="#">Something else here</a></li>
+//   </ul>
+//     <form action="/" method="get">
+//         <label htmlFor="header-search">
+//             <span className="visually-hidden">Search blog posts</span>
+//         </label>
+//         <input
+//             type="text"
+//             id="header-search"
+//             placeholder="Search"
+//             name="s" 
+//         />
+//         <button type="submit">Search</button>
+//     </form>
+//     </div>
+
+
+{/* <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container-fluid">
+  <a class="navbar-brand" href="app.js">Asia's Flashcards!</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="app.js">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="app.js">Flashcards</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="app.js">Create a New Deck of Cards </a>
+      </li>
+    </ul>
+    <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="search" />
+            <button class="butn btn-outline-success" type="submit">Search</button>
     </form>
+  </div>
+</div>
+</nav> */}
+
 );
 
-export default SearchBar;
+// export default SearchBar;
